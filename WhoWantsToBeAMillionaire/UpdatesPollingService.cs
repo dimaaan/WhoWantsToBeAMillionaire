e41a53762e7d438ca84564d094262fdcaa706ba1
dimaaan <dimaaan@gmail.com>
2019-12-26 14:54:57 +0300

    Refactoring

--- WhoWantsToBeAMillionaire/UpdatesPollingService.cs
+++ WhoWantsToBeAMillionaire/UpdatesPollingService.cs
@@ -41,7 +41,7 @@ class UpdatesPollingService : BackgroundService
                 }
             }
         }
-        catch (TaskCanceledException _)
+        catch (TaskCanceledException)
         {
         }
 
