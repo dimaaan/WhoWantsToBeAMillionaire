2a32f1af68d6ab24c8e73b0a6269fd1dbf729f69
dimaaan <dimaaan@gmail.com>
2019-12-26 14:58:24 +0300

    fix speech bug

--- WhoWantsToBeAMillionaire/GameService.cs
+++ WhoWantsToBeAMillionaire/GameService.cs
@@ -211,7 +211,6 @@ class GameService
             template,
             question.RightAnswer,
             question.RightAnswerText,
-            ScoreTable[level + 1],
             ScoreTable[level]
         );
     }
